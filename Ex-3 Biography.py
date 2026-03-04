608d0b61d414a4d565f9c4c862d829db0769f958
cyber-joel <joelvarghesegeorge003j@gmail.com>
2026-03-05 02:28:35 +0400

    Update Ex-3 Biography.py

---
+++ Ex-3 Biography.py
@@ -0,0 +1,16 @@
+# Ask The User For Their Information
+name = input("Enter your full name: ")
+hometown = input("Enter your hometown: ")
+age = input("Enter your age: ")
+
+# Store In Dictionary
+person_info = {"Name": name, "Hometown": hometown}
+
+if age.isdigit():
+    person_info["Age"] = int(age)
+else:
+    print("Enter a valid age")
+
+# Print Only The Available Information
+for key in person_info:
+    print(person_info[key], sep="\n")
