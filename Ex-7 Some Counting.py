f891b2216cd68ff7e1ec8b9b3c660d134a21d5f4
cyber-joel <joelvarghesegeorge003j@gmail.com>
2026-03-06 03:21:28 +0400

    Exercise 7

---
+++ Ex-7 Some Counting.py
@@ -0,0 +1,33 @@
+# Count up from 0 to 50
+print("! Count Up From 0 To 50 !")
+print("")
+for i in range(0, 51):
+    print(i)
+
+# Count down from 50 to 0
+print("")
+print("! Count Up From 50 To 0 !")
+print("")
+for i in range(50, -1, -1):
+    print(i)
+
+# Count up from 30 to 50
+print("")
+print("! Count Up From 30 To 50 !")
+print("")
+for i in range(30, 51):
+    print(i)
+
+# Count down from 50 to 10 in steps of 2
+print("")
+print("! Count Up From 50 To 10 In Steps Of 2 !")
+print("")
+for i in range(50, 9, -2):
+    print(i)
+
+# Count up from 100 to 200 in steps of 5
+print("")
+print("! Count Up From 100 To 200 In Steps Of 5 !")
+print("")
+for i in range(100, 201, 5):
+    print(i)
